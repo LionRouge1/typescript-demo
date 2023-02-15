55406375dde11065b5802467051e825fb5f70706
Masoudi Parrot <masoudi@localhost.localdomain>
2023-02-14 19:17:02 -0500

    render new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Status } from './components/Status';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      <Status status='success' />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
+      </Oscar>
+      <Greet name='Matchoudi' isLoggedIn={false} />
     </div>
   );
 }
