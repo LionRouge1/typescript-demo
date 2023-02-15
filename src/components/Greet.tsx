167ed357e1b203e0cada1d10bb866dbbf6bd0635
Masoudi Parrot <masoudi@localhost.localdomain>
2023-02-14 19:13:37 -0500

    add new change to Greet component

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,7 +1,20 @@
-export const Greet = () => {
+type GreetProps = {
+    name: string,
+    messageCount?: number,
+    isLoggedIn: boolean
+}
+
+export const Greet = (props:GreetProps) => {
+    const { name, messageCount = 0, isLoggedIn } = props
     return (
         <div>
-            <h2>Welcome Matchoudi! You have 10 unread messages</h2>
+            <h2>
+                {
+                    isLoggedIn
+                    ? `Welcome ${name}! You have ${messageCount} unread messages`
+                    : 'Welcome guest'
+                }
+            </h2>
         </div>
     )
 }
