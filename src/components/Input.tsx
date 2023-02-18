b242382de3eee0aecfdf90e700b10b340f81bed4
Masoudi Parrot <masoudi@localhost.localdomain>
2023-02-18 17:38:19 -0500

    Create input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+type InputProps = {
+    value: string,
+    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const Input = ({ value, handleChange }: InputProps) => {
+    return <input type="text" value={value} onChange={handleChange} />
+}
